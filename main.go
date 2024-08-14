05cb9638a65a0c072ec52407b5a0e8b5a4fe84a2
subashshakya <subashshakya12@gmail.com>
2024-08-14 09:02:32 -0400

    feat: add databaseConnection

--- main.go
+++ main.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/subashshakya/SFSS/db/connection"
+	"github.com/subashshakya/SFSS/db/orms"
 )
 
 func main() {
-	_, err := connection.CreateDatabaseConnection()
+	IP := os.Getenv("IP")
+	PORT := os.Getenv("PORT")
+	serverConfig := fmt.Sprintf("%s/%s", IP, PORT)
+	fmt.Print(IP, PORT)
+	db, err := connection.CreateDatabaseConnection()
+	r := gin.Default()
+	r.Use()
+	if err == nil {
+		fmt.Println("DB Connection Successful")
+	} else {
+		fmt.Println("Error => ", err)
+	}
+	orms.DatabaseConnection = db
+	dbConn, err := db.DB()
 	if err != nil {
-		fmt.Print("DB Connection Successful")
+		panic(err)
+	}
+	defer dbConn.Close()
+	serverRunErr := r.Run(serverConfig)
+	if serverRunErr != nil {
+		panic(serverRunErr)
 	}
 }
