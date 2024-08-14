7b73da509ca176dd8d927e53c4e3a8a08ee2f279
subashshakya <subashshakya12@gmail.com>
2024-08-14 09:01:14 -0400

    refactor: use environment file instead of hardcoding cred

--- db/connection/connection.go
+++ db/connection/connection.go
@@ -1,18 +1,31 @@
 package connection
 
 import (
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
 	"fmt"
+	"os"
 )
 
 func CreateDatabaseConnection() (*gorm.DB, error) {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		return nil, err
+	}
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_PORT := os.Getenv("DB_PORT")
+
+	postgresConfig := fmt.Sprintf("host=%s user=%s password=%s port=%s sslmode=disable", DB_HOST, DB_USER, DB_PASSWORD, DB_PORT)
+
+	fmt.Printf("postgres config => %s connection.go ", postgresConfig)
 	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "host=172.17.0.2 user=postgres password=12345678 port=5432 sslmode=disable",
+		DSN:                  postgresConfig,
 		PreferSimpleProtocol: true,
 	}), &gorm.Config{})
-	fmt.Print(db)
-	fmt.Printf("\n")
 	return db, err
 }
