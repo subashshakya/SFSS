0aeda111795463edf1716bc4f2db928ee371e755
subashshakya <subashshakya12@gmail.com>
2024-08-14 09:04:18 -0400

    chore: initialize controllers

---
+++ controllers/user.controllers.go
@@ -0,0 +1,23 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func UserSignUp(c *gin.Context) {
+
+}
+
+func UserSignIn(c *gin.Context) {
+
+}
+
+func GetUser(c *gin.Context) {
+
+}
+
+func UpdateUser(c *gin.Context) {
+
+}
+
+func DeleteUser(c *gin.Context) {}
