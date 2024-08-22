ab69d90bdb95768200b0158e805ea80e0059fb76
subashshakya <subashshakya12@gmail.com>
2024-08-21 23:21:26 -0400

    feat: add constants

---
+++ constants/string.constants.go
@@ -0,0 +1,6 @@
+package constants
+
+const InternalServerError = "Internal Server Error"
+const BadRequest = "Invalid request"
+const Unauthorized = "Unauthorized Access Detected"
+const ValidationError = "Validation Error Occured"
