92f28c547cbca4da9351b086b643472a0ec9092b
subashshakya <subashshakya12@gmail.com>
2024-08-25 21:54:00 -0400

    chore: add constants

--- constants/string.constants.go
+++ constants/string.constants.go
@@ -1,6 +1,14 @@
 package constants
 
+import "time"
+
 const InternalServerError = "Internal Server Error"
-const BadRequest = "Invalid request"
+const BadRequest = "Invalid Request"
 const Unauthorized = "Unauthorized Access Detected"
 const ValidationError = "Validation Error Occured"
+const IdCannotBeZero = "ID cannot be zero"
+const UUIDInvalid = "UUID is invalid"
+const IdEmpty = "ID field is empty"
+const ShortTimeout = time.Second * 5
+const LongTimeout = time.Second * 7
+const NotFound = "Could not find requested object"
